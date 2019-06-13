610f60073e2e943537d0c7296b53c66c187393d4
Ivan5 <converse_ivan@hotmail.com>
2019-06-13 12:41:19 -0500

    Make a Navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav>
+    <v-toolbar app dark class="secondary">
+      <v-toolbar-side-icon @click="isDrawerOpen = !isDrawerOpen"></v-toolbar-side-icon>
+      <v-toolbar-title class="headline text-uppercase">
+        <span class="font-weight-black primary--text">Ivan's</span>
+        <span class="font-weight-light">Timer</span>
+      </v-toolbar-title>
+    </v-toolbar>
+
+    <v-navigation-drawer dark app v-model="isDrawerOpen">
+      <v-toolbar class="secondary">
+        <v-toolbal-title class="title">Laps</v-toolbal-title>
+      </v-toolbar>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {
+      isDrawerOpen: false
+    };
+  }
+};
+</script>
+
+<style>
+</style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,11 @@
-import Vue from 'vue'
-import Vuetify from 'vuetify/lib'
-import 'vuetify/src/stylus/app.styl'
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
+import "vuetify/src/stylus/app.styl";
 
 Vue.use(Vuetify, {
-  iconfont: 'md',
-})
+  iconfont: "md",
+  theme: {
+    primary: "#FFCE00",
+    secondary: "#171717"
+  }
+});
