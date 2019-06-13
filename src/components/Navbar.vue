4beda4f0d88398d6656fb64b4f0d69eec1a52617
Ivan5 <converse_ivan@hotmail.com>
2019-06-13 14:35:50 -0500

    Add functionality of start, pause, stop and make a lap!

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -12,6 +12,12 @@
       <v-toolbar class="secondary">
         <v-toolbal-title class="title">Laps</v-toolbal-title>
       </v-toolbar>
+      <v-list>
+        <v-list-tile
+          v-for="(lap,index) in laps"
+          :key="index"
+        >Lap {{index + 1 }} : {{ lap.formattedTime }}</v-list-tile>
+      </v-list>
     </v-navigation-drawer>
   </nav>
 </template>
@@ -19,6 +25,7 @@
 <script>
 export default {
   name: "Navbar",
+  props: ["laps"],
   data() {
     return {
       isDrawerOpen: false
